bd04ecf6d57cc4710e53f0a9a795893eff441244
bpal938 <69737582+bpal938@users.noreply.github.com>
2021-10-03 17:04:18 +1300

    Added adaptor classes

---
+++ app/src/main/java/com/example/gamesearch/adaptors/SearchItemAdaptor.java
@@ -0,0 +1,15 @@
+package com.example.gamesearch.adaptors;
+
+import android.content.Context;
+
+import com.example.gamesearch.interfaces.Product;
+
+import java.util.List;
+
+public class SearchItemAdaptor {
+
+    private int layoutId;
+    private List<Product> productList;
+    private Context context;
+
+}
