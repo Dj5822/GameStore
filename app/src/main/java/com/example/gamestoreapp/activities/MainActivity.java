d4eacddb19dd0645c0511949e273204368fb8087
jlob275 <68893201+jlob275@users.noreply.github.com>
2021-10-07 21:49:14 +1300

    CategoryClickListener to open a category from MainActivity implemented

--- app/src/main/java/com/example/gamestoreapp/activities/MainActivity.java
+++ app/src/main/java/com/example/gamestoreapp/activities/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.gamestoreapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import android.os.Bundle;
 import com.example.gamestoreapp.R;
+import com.example.gamestoreapp.interfaces.ListActivity;
 import com.example.gamestoreapp.interfaces.Store;
 import com.example.gamestoreapp.listeners.CategoryClickListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Store store;
     private CategoryClickListener listener;
+
+    private class ViewHolder {
+        CardView actionCardView, strategyCardView, casualCardView, simulationCardView;
+        public ViewHolder() {
+            actionCardView = findViewById(R.id.card_view_action);
+            strategyCardView = findViewById(R.id.card_view_strategy);
+            casualCardView = findViewById(R.id.card_view_casual);
+            simulationCardView = findViewById(R.id.card_view_simulation);
+        }
+    }
 
     void startSearch(){
 
@@ -19,5 +34,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewHolder vh = new ViewHolder();
+
+        vh.actionCardView.setOnClickListener(new CategoryClickListener(ActionListActivity.class));
     }
 }
--- app/src/main/java/com/example/gamestoreapp/listeners/CategoryClickListener.java
+++ app/src/main/java/com/example/gamestoreapp/listeners/CategoryClickListener.java
@@ -1,10 +1,25 @@
 package com.example.gamestoreapp.listeners;
 
+import android.content.Intent;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gamestoreapp.activities.ActionListActivity;
+
 public class CategoryClickListener implements View.OnClickListener {
+
+    Class<?> activity;
+
+    public CategoryClickListener(Class<?> activity) {
+        super();
+        this.activity = activity;
+    }
+
     @Override
     public void onClick(View view) {
-        return;
+        Intent categoryOpenIntent = new Intent(view.getContext(), activity);
+        categoryOpenIntent.putExtra("MessageFromMainActivity", "This message came from Main Activity");
+        view.getContext().startActivity(categoryOpenIntent);
     }
 }
