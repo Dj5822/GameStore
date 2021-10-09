b7b35e2e246a0168235ce061af4404cf387b5d22
bpal938 <69737582+bpal938@users.noreply.github.com>
2021-10-09 13:38:36 +1300

    Created viewholder for details activity

--- app/src/main/java/com/example/gamestoreapp/activities/DetailsActivity.java
+++ app/src/main/java/com/example/gamestoreapp/activities/DetailsActivity.java
@@ -3,14 +3,45 @@ package com.example.gamestoreapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.gamestoreapp.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private ViewHolder vh;
+
+    private class ViewHolder{
+        ImageView gameIcon;
+        TextView gameName, studioName, viewCount, soldCount, detailsText;
+        Button purchaseButton, imageSwitchButton1, imageSwitchButton2, imageSwitchButton3, imageSwitchButton4;
+        ImageSwitcher gameImageSwitcher;
+
+        public ViewHolder(){
+            gameIcon = findViewById(R.id.gameIcon);
+            gameName = findViewById(R.id.gameName);
+            studioName = findViewById(R.id.studioName);
+            viewCount = findViewById(R.id.viewCount);
+            soldCount = findViewById(R.id.soldCount);
+            detailsText = findViewById(R.id.detailsText);
+            purchaseButton = findViewById(R.id.purchaseButton);
+            imageSwitchButton1 = findViewById(R.id.imageSwitchButton1);
+            imageSwitchButton2 = findViewById(R.id.imageSwitchButton2);
+            imageSwitchButton3 = findViewById(R.id.imageSwitchButton3);
+            imageSwitchButton4 = findViewById(R.id.imageSwitchButton4);
+            gameImageSwitcher = findViewById(R.id.gameImageSwitcher);
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        vh = new ViewHolder();
     }
 }
