05207af797dfd92d4cbf656655a58cb110d8cbb0
jlob275 <68893201+jlob275@users.noreply.github.com>
2021-10-11 04:02:54 +1300

    Implemented Casual category

--- app/src/main/java/com/example/gamestoreapp/activities/CasualListActivity.java
+++ app/src/main/java/com/example/gamestoreapp/activities/CasualListActivity.java
@@ -2,29 +2,34 @@ package com.example.gamestoreapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import com.example.gamestoreapp.R;
 import com.example.gamestoreapp.interfaces.ListActivity;
 
-public class CasualListActivity extends AppCompatActivity implements ListActivity {
+public class CasualListActivity extends CategoryListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casual_list);
-    }
 
-    @Override
-    public void loadCategory() {
+        ProgressBar progressBar = findViewById(R.id.load_progressbar_casual);
+        ListView listView = findViewById(R.id.list_view_casual);
+        LinearLayout actionLayout = findViewById(R.id.casual_layout);
+        ImageView categoryImageView = findViewById(R.id.header_image_casual);
+        TextView nextImageIcon = findViewById(R.id.next_image_icon_casual);
+        TextView prevImageIcon = findViewById(R.id.prev_image_icon_casual);
 
-    }
+        vh = new ViewHolder(listView, progressBar, actionLayout,
+                categoryImageView, nextImageIcon, prevImageIcon);
 
-    @Override
-    public void goToNextCategory() {
+        categoryName = "casual";
 
-    }
-
-    @Override
-    public void goToPreviousCategory() {
-
+        loadCategory();
     }
 }
