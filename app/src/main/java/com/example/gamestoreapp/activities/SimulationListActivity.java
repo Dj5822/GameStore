cd4f9117ef2b902365dd98472eb4e12c922206fa
jlob275 <68893201+jlob275@users.noreply.github.com>
2021-10-12 20:18:17 +1300

    Implemented SimulationListActivity

--- app/src/main/java/com/example/gamestoreapp/activities/SimulationListActivity.java
+++ app/src/main/java/com/example/gamestoreapp/activities/SimulationListActivity.java
@@ -2,29 +2,34 @@ package com.example.gamestoreapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import com.example.gamestoreapp.R;
 import com.example.gamestoreapp.interfaces.ListActivity;
 
-public class SimulationListActivity extends AppCompatActivity implements ListActivity {
+public class SimulationListActivity extends CategoryListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulation_list);
-    }
 
-    @Override
-    public void loadCategory() {
+        ProgressBar progressBar = findViewById(R.id.load_progressbar_simulation);
+        ListView listView = findViewById(R.id.list_view_simulation);
+        LinearLayout actionLayout = findViewById(R.id.simulation_layout);
+        ImageView categoryImageView = findViewById(R.id.header_image_simulation);
+        TextView nextImageIcon = findViewById(R.id.next_image_icon_simulation);
+        TextView prevImageIcon = findViewById(R.id.prev_image_icon_simulation);
 
-    }
+        vh = new ViewHolder(listView, progressBar, actionLayout,
+                categoryImageView, nextImageIcon, prevImageIcon);
 
-    @Override
-    public void goToNextCategory() {
+        categoryName = "simulation";
 
-    }
-
-    @Override
-    public void goToPreviousCategory() {
-
+        loadCategory();
     }
 }
