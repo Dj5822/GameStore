534207ab9103fecafa585e0cb2ad703c9e9eca35
dj5822 <Dj5822@gmail.com>
2021-10-13 15:25:08 +1300

    Discarded redundant views

--- app/src/main/java/com/example/gamestoreapp/activities/SimulationListActivity.java
+++ app/src/main/java/com/example/gamestoreapp/activities/SimulationListActivity.java
@@ -16,14 +16,17 @@ public class SimulationListActivity extends CategoryListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simulation_list);
+        setContentView(R.layout.activity_list);
 
-        ProgressBar progressBar = findViewById(R.id.load_progressbar_simulation);
-        ListView listView = findViewById(R.id.list_view_simulation);
-        LinearLayout actionLayout = findViewById(R.id.simulation_layout);
-        ImageView categoryImageView = findViewById(R.id.header_image_simulation);
-        TextView nextImageIcon = findViewById(R.id.next_image_icon_simulation);
-        TextView prevImageIcon = findViewById(R.id.prev_image_icon_simulation);
+        ProgressBar progressBar = findViewById(R.id.load_progressbar);
+        ListView listView = findViewById(R.id.list_view);
+        LinearLayout actionLayout = findViewById(R.id.list_layout);
+        ImageView categoryImageView = findViewById(R.id.header_image);
+        TextView nextImageIcon = findViewById(R.id.next_image_icon);
+        TextView prevImageIcon = findViewById(R.id.prev_image_icon);
+
+        TextView title = findViewById(R.id.category_title);
+        title.setText("Simulation");
 
         vh = new ViewHolder(listView, progressBar, actionLayout,
                 categoryImageView, nextImageIcon, prevImageIcon);
